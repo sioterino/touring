2d11d51026ac04640001f8238c4136691561277f
sioterino <sofialvestoreti@gmail.com>
2025-11-25 16:11:08 -0300

    add/simple api call for testing purposes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
     <HashRouter>
       <Routes>
-        <Route path='/' element={ <h1>ROOT</h1> } />
+        <Route path='/' element={ <HomePage /> } />
         <Route path='/all' element={ <h1>ALL</h1> } />
         <Route path='*' element={ <h1>404 NOT FOUND</h1> } />
       </Routes>
